86dd9e47d0775b7f8c232119773055605387da42
GROSVENOR\gsaharia <gsaharia@gcmlp.com>
2021-12-06 17:53:55 -0500

    add first simple excel renderer

--- PythonPackage/src/gcm/InvestmentsReporting/Runners/investmentsreporting.py
+++ PythonPackage/src/gcm/InvestmentsReporting/Runners/investmentsreporting.py
@@ -1,4 +1,6 @@
 from gcm.ProgramRunner.programrunner import ProgramRunner
+import pandas as pd
+import datetime as dt
 
 
 class InvestemtnsReportingRunner(ProgramRunner):
@@ -6,8 +8,32 @@ class InvestemtnsReportingRunner(ProgramRunner):
     def base_container(self):
         pass
 
+    def print_single_data_frame(
+        self,
+        asofdate: dt.datetime,
+        input_data: pd.DataFrame,
+        location: str,
+        file_name: str,
+    ):
+        date_string = asofdate.strftime("%Y-%m-%d")
+        final_outcome = f"{location}{file_name}_{date_string}.xlsx"
+        input_data.to_excel(final_outcome)
+        pass
+
     def run(self, **kwargs):
-        return "Running Report"
+        asofdate: dt.datetime = kwargs["asofdate"]
+        input_data = kwargs["input_data"]
+        print_type: str = kwargs["print_type"]
+        if type(input_data) is pd.DataFrame and print_type == "simple":
+            location: str = kwargs["location"]
+            file_name: str = kwargs["file_name"]
+            self.print_single_data_frame(
+                asofdate=asofdate,
+                input_data=input_data,
+                location=location,
+                file_name=file_name,
+            )
+        return "done"
 
     def global_post_conditions(self):
         return super().global_post_conditions()
--- PythonPackage/test_gcm_investmentsreporting.py
+++ PythonPackage/test_gcm_investmentsreporting.py
@@ -1,8 +1,23 @@
 from src.gcm.InvestmentsReporting.Runners.investmentsreporting import (
     InvestemtnsReportingRunner,
 )
+import pandas as pd
+import datetime as dt
 
 
-def test_helloworld_no_params():
-    v = InvestemtnsReportingRunner().execute()
+def test_simple_print():
+    data = {
+        "Name": ["1", "2", "3", "4"],
+        "Age": [20, 21, 19, 18],
+    }
+    df = pd.DataFrame(data)
+    asofdate = dt.datetime(2021, 11, 30)
+    file_name = "Test_Data"
+    v = InvestemtnsReportingRunner().execute(
+        asofdate=asofdate,
+        input_data=df,
+        print_type="simple",
+        location="C:/Temp/",
+        file_name=file_name,
+    )
     assert v is not None
