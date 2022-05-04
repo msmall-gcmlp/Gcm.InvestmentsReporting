4f447956dca644074c9f628585b10c4c4b98a7f3
Cullen McNamee <cmcnamee@gcmlp.com>
2022-05-04 15:44:49 -0500

    updated conftest

--- PythonPackage/tests/gcm/inv/reporting/conftest.py
+++ PythonPackage/tests/gcm/inv/reporting/conftest.py
@@ -8,6 +8,7 @@ def performance_quality_report_inputs():
     f = open(os.path.dirname(__file__) + "\\test_data\\performance_quality_report_inputs.json")
     return json.load(f)
 
+
 @pytest.fixture
 def performance_quality_report_inputs_all():
     f = open(os.path.dirname(__file__) + "\\test_data\\performance_quality_report_inputs_all.json")
