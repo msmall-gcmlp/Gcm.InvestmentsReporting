d67fad09925480e4da825c655c094ca8d9e14341
GROSVENOR\gsaharia <gsaharia@gcmlp.com>
2022-05-26 13:42:40 -0400

    remove two files

--- PythonPackage/src/gcm/inv/reporting/core/Utils/aggregate_file_utils.py
+++ PythonPackage/src/gcm/inv/reporting/core/Utils/aggregate_file_utils.py
@@ -64,9 +64,7 @@ def merge_metadata_to_target_file(
 ) -> None:
 
     ordered_values = [
-        v
-        for d in itertools.chain(metadata_list)
-        for _, v in sorted(d.items())
+        v for d in itertools.chain(metadata_list) for _, v in sorted(d.items())
     ]
     result = dict(zip(metadata_list.count(1), ordered_values))
     copy_metadata(
--- PythonPackage/src/run_performance_quality_report.py
+++ PythonPackage/src/run_performance_quality_report.py
@@ -1,17 +1,24 @@
 import datetime as dt
 import json
 from pandas._libs.tslibs.offsets import relativedelta
-from gcm.inv.reporting.reports.aggregate_performance_quality_report import AggregatePerformanceQualityReport
-from gcm.inv.reporting.reports.performance_quality_peer_summary_report import PerformanceQualityPeerSummaryReport
-from gcm.inv.reporting.reports.performance_quality_report_data import PerformanceQualityReportData
-from gcm.inv.reporting.reports.performance_quality_report import PerformanceQualityReport
+from gcm.inv.reporting.reports.aggregate_performance_quality_report import (
+    AggregatePerformanceQualityReport,
+)
+from gcm.inv.reporting.reports.performance_quality_peer_summary_report import (
+    PerformanceQualityPeerSummaryReport,
+)
+from gcm.inv.reporting.reports.performance_quality_report_data import (
+    PerformanceQualityReportData,
+)
+from gcm.inv.reporting.reports.performance_quality_report import (
+    PerformanceQualityReport,
+)
 from gcm.Dao.DaoRunner import DaoRunner, DaoRunnerConfigArgs
 from gcm.Dao.DaoSources import DaoSource
 from gcm.inv.reporting.reports.report_binder import ReportBinder
 
 
 class RunPerformanceQualityReports:
-
     def __init__(self, as_of_date):
         self._runner = DaoRunner(
             container_lambda=lambda b, i: b.config.from_dict(i),
@@ -24,11 +31,12 @@ class RunPerformanceQualityReports:
                     DaoSource.ReportingStorage.name: {
                         "Environment": "uat",
                         "Subscription": "nonprd",
-                    }
+                    },
                 }
-            })
+            },
+        )
         self._as_of_date = as_of_date
-        self._params = {'status': 'EMM', 'vertical': 'ARS', 'entity': 'PFUND'}
+        self._params = {"status": "EMM", "vertical": "ARS", "entity": "PFUND"}
 
     def generate_report_data(self, investment_ids):
         perf_quality_data = PerformanceQualityReportData(
@@ -36,7 +44,8 @@ class RunPerformanceQualityReports:
             start_date=self._as_of_date - relativedelta(years=10),
             end_date=self._as_of_date,
             as_of_date=self._as_of_date,
-            investment_ids=investment_ids)
+            investment_ids=investment_ids,
+        )
         return perf_quality_data.execute()
 
     def generate_peer_summaries(self, peer_groups):
@@ -44,40 +53,59 @@ class RunPerformanceQualityReports:
             perf_quality_data = PerformanceQualityPeerSummaryReport(
                 runner=self._runner,
                 as_of_date=self._as_of_date,
-                peer_group=peer_group)
+                peer_group=peer_group,
+            )
             perf_quality_data.execute()
 
     def generate_fund_reports(self, fund_names):
         for fund in fund_names:
             params = self._params.copy()
-            params['fund_name'] = fund
-            perf_quality_report = PerformanceQualityReport(runner=self._runner, as_of_date=self._as_of_date,
-                                                           fund_name=fund)
+            params["fund_name"] = fund
+            perf_quality_report = PerformanceQualityReport(
+                runner=self._runner,
+                as_of_date=self._as_of_date,
+                fund_name=fund,
+            )
             perf_quality_report.execute()
 
     def combine_by_portfolio(self, portfolio_acronyms=None):
-        agg_perf_quality = ReportBinder(runner=self._runner, as_of_date=self._as_of_date,
-                                        portfolio_acronyms=portfolio_acronyms)
+        agg_perf_quality = ReportBinder(
+            runner=self._runner,
+            as_of_date=self._as_of_date,
+            portfolio_acronyms=portfolio_acronyms,
+        )
         agg_perf_quality.execute()
 
     def agg_perf_quality_by_portfolio(self, portfolio_acronyms=None):
-        agg_perf_quality = AggregatePerformanceQualityReport(runner=self._runner, as_of_date=self._as_of_date,
-                                                             acronyms=portfolio_acronyms)
+        agg_perf_quality = AggregatePerformanceQualityReport(
+            runner=self._runner,
+            as_of_date=self._as_of_date,
+            acronyms=portfolio_acronyms,
+        )
         agg_perf_quality.execute()
 
 
 if __name__ == "__main__":
-    report_runner = RunPerformanceQualityReports(as_of_date=dt.date(2022, 3, 31))
+    report_runner = RunPerformanceQualityReports(
+        as_of_date=dt.date(2022, 3, 31)
+    )
     funds_and_peers = report_runner.generate_report_data(investment_ids=None)
 
     funds_and_peers = json.loads(funds_and_peers)
-    fund_names = funds_and_peers.get('fund_names')
-    peer_groups = funds_and_peers.get('peer_groups')
+    fund_names = funds_and_peers.get("fund_names")
+    peer_groups = funds_and_peers.get("peer_groups")
 
     report_runner.generate_peer_summaries(peer_groups=peer_groups)
-    report_runner.generate_fund_reports(fund_names=['Citadel', 'Citadel Global Equities',
-                                                    'D1 Capital', 'Element', 'Elliott'])
-    report_runner.agg_perf_quality_by_portfolio(portfolio_acronyms=['GIP'])
+    report_runner.generate_fund_reports(
+        fund_names=[
+            "Citadel",
+            "Citadel Global Equities",
+            "D1 Capital",
+            "Element",
+            "Elliott",
+        ]
+    )
+    report_runner.agg_perf_quality_by_portfolio(portfolio_acronyms=["GIP"])
     report_runner.combine_by_portfolio()
 
     # TODO - Incorporate changes to ReportRunner (add required tags)
