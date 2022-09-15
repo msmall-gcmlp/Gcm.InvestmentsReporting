4a51efbda466b6fa17d12654cb704b886a1fab46
Cullen McNamee <cmcnamee@gcmlp.com>
2022-09-15 11:41:02 -0500

    added eof rba report orchestrator

---
+++ AzureFunctions/EofRbaReportActivity/__init__.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from gcm.Scenario.scenario import Scenario
+from Reports._adhoc_scripts.run_eof_rba_report import RunEofReturnBasedAttributionReport
+from gcm.Dao.DaoRunner import DaoRunner
+from gcm.inv.quantlib.enum_source import PeriodicROR
+
+
+def main(requestBody) -> str:
+    params = requestBody["params"]
+    run = params["run"]
+    asofdate = params["asofdate"]
+    periodicity = params['periodicity']
+    as_of_date = datetime.strptime(asofdate, "%Y-%m-%d").date()
+    runner = DaoRunner()
+
+    if periodicity == 'ITD':
+        periodicity = PeriodicROR.ITD
+    elif periodicity == 'YTD':
+        periodicity = PeriodicROR.YTD
+
+    if run == "EofRbaReport":
+        with Scenario(runner=runner, as_of_date=as_of_date, periodicity=periodicity):
+            eof_rba = RunEofReturnBasedAttributionReport()
+            return eof_rba.execute()
