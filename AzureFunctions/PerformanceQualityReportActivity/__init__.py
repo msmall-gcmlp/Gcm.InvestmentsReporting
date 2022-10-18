b872230a1ee26b91638fad9e52be2672a8caa06f
Cullen McNamee <cmcnamee@gcmlp.com>
2022-10-18 13:20:26 -0500

    bug fix for imports

--- AzureFunctions/PerformanceQualityReportActivity/__init__.py
+++ AzureFunctions/PerformanceQualityReportActivity/__init__.py
@@ -1,13 +1,13 @@
 import ast
 from datetime import datetime
 from gcm.inv.scenario import Scenario
-from Reports.reports.performance_quality_report_data import (
+from Reports.reports.performance_quality.performance_quality_report_data import (
     PerformanceQualityReportData,
 )
-from Reports.reports.performance_quality_peer_summary_report import (
+from Reports.reports.performance_quality.performance_quality_peer_summary_report import (
     PerformanceQualityPeerSummaryReport,
 )
-from Reports.reports.performance_quality_report import (
+from Reports.reports.performance_quality.performance_quality_report import (
     PerformanceQualityReport,
 )
 from gcm.Dao.DaoRunner import DaoRunner
