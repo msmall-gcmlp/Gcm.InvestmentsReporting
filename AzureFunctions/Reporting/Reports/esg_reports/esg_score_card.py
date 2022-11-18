0dbc4d14384251262336d15746dafcacc126bef4
GROSVENOR\gsaharia <gsaharia@gcmlp.com>
2022-11-18 11:41:49 -0500

    meta tags

--- AzureFunctions/Reporting/Reports/esg_reports/esg_score_card.py
+++ AzureFunctions/Reporting/Reports/esg_reports/esg_score_card.py
@@ -45,11 +45,38 @@ class ESG_ScoreCard(ReportStructure):
     @property
     def save_params(self) -> tuple[dict, DaoSource]:
         dt_name = Scenario.get_attribute("as_of_date").strftime("%Y-%m-%d")
+        runner: DaoRunner = Scenario.get_attribute("dao")
+        data: pd.DataFrame = runner.execute(
+            params={
+                "table": "contributions",
+                "schema": "reporting21",
+            },
+            source=DaoSource.InvestmentsDwh,
+            operation=lambda dao, runner: dao.get_data(runner),
+        )
+        data = data[data["EntityId"].isin([911, 923])]
+        data = data[data["IndicatorId"].isin([255, 253])]
+        data = data[["IndicatorId", "Value"]]
+        data.drop_duplicates(inplace=True)
+        data["Value"] = data["Value"].apply(
+            lambda x: x.replace("\n", "").replace("\r", "")
+        )
+        data.reset_index(inplace=True, drop=True)
+        temp_dict = {
+            253: "Primary_GCM_Coverage_Email",
+            255: "Alternate_GCM_Coverage_Email",
+        }
+        d_final = {}
+        for i in range(0, data.shape[0]):
+            item = data.loc[i].to_dict()
+            assert item is not None
+            d_final[temp_dict[item["IndicatorId"]]] = item["Value"]
+        metadata = d_final | self.metadata()
         return (
             AzureDataLakeDao.create_get_data_params(
                 f"{ESG_ScoreCard._output_directory}/",
                 f"ESG_Report_Test_{dt_name}.xlsx",
-                metadata=self.metadata(),
+                metadata=metadata,
             ),
             DaoSource.DataLake,
         )
