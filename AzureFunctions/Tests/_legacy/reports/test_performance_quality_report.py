00e04c610ac61710f8ae2da283763810585f74b7
datimmer <datimmer@gcmlp.com>
2022-12-08 13:37:22 -0600

    fix test

--- AzureFunctions/Tests/_legacy/reports/test_performance_quality_report.py
+++ AzureFunctions/Tests/_legacy/reports/test_performance_quality_report.py
@@ -83,7 +83,7 @@ class TestPerformanceQualityReport:
         gcm_peer_columns = pd.MultiIndex.from_tuples(gcm_peer_columns, names=["PeerGroupName", "SourceInvestmentId"])
         gcm_peer_constituent_returns.columns = gcm_peer_columns
 
-        eh_inputs = report_inputs['eurekahedge_inputs']['EHI100 Long/Short Equity']
+        eh_inputs = report_inputs['eurekahedge_inputs']["EHI100 Long-Biased Fundamental Equity"]
         eurekahedge_returns = pd.read_json(eh_inputs['eurekahedge_returns'], orient='index')
         eurekahedge_constituent_returns = pd.read_json(eh_inputs['eurekahedge_constituent_returns'], orient='index')
         eh_columns = [ast.literal_eval(x) for x in eurekahedge_constituent_returns.columns]
