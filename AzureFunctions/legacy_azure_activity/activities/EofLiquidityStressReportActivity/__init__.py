24f1a7b7e722fd9563bbfd045a638c4e6ccef3a5
GROSVENOR\agalstyan <agalstyan@gcmlp.com>
2023-01-12 19:29:58 -0500

    Develop fix dao call

--- AzureFunctions/legacy_azure_activity/activities/EofLiquidityStressReportActivity/__init__.py
+++ AzureFunctions/legacy_azure_activity/activities/EofLiquidityStressReportActivity/__init__.py
@@ -5,9 +5,9 @@ from _legacy.Reports.reports.eof_liquidity_stress.report_data import (
 from _legacy.Reports.reports.eof_liquidity_stress.eof_liquidity_stress_report import (
     EofLiquidityReport,
 )
-from gcm.Dao.DaoRunner import DaoRunner, DaoRunnerConfigArgs
+# from gcm.Dao.DaoRunner import DaoRunner, DaoRunnerConfigArgs
 from gcm.inv.scenario import Scenario
-from gcm.Dao.DaoSources import DaoSource
+# from gcm.Dao.DaoSources import DaoSource
 
 
 def main(requestBody) -> str:
@@ -15,29 +15,29 @@ def main(requestBody) -> str:
     run = params["run"]
     as_of_date = params["as_of_date"]
     as_of_date = datetime.strptime(as_of_date, "%Y-%m-%d").date()
-    config_params = {
-        DaoRunnerConfigArgs.dao_global_envs.name: {
-            DaoSource.InvestmentsDwh.name: {
-                "Environment": "prd",
-                "Subscription": "prd",
-            }
-        }
-    }
+    # config_params = {
+    #     DaoRunnerConfigArgs.dao_global_envs.name: {
+    #         DaoSource.InvestmentsDwh.name: {
+    #             "Environment": "prd",
+    #             "Subscription": "prd",
+    #         }
+    #     }
+    # }
 
     if run == "RunEofLiquidityStress":
-        with Scenario(dao_config=config_params, as_of_date=as_of_date).context():
+        with Scenario(as_of_date=as_of_date).context():
             input_data = EofStressTestingData(
                 runner=Scenario.get_attribute("dao"),
                 as_of_date=params["as_of_date"],
                 scenario=["Liquidity Stress"],
             ).execute()
-            runner2 = DaoRunner(
-                container_lambda=lambda b, i: b.config.from_dict(i),
-                config_params=config_params,
-            )
+            # runner2 = DaoRunner(
+            #     container_lambda=lambda b, i: b.config.from_dict(i),
+            #     config_params=config_params,
+            # )
 
             eof_liquidity = EofLiquidityReport(
-                runner=runner2,
+                runner=Scenario.get_attribute("dao"),
                 as_of_date=as_of_date,
                 factor_inventory=input_data,
                 manager_exposure=input_data,
