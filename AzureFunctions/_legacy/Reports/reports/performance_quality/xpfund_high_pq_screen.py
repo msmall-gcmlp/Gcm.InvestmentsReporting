c31969d5fa3f5e538c6d807c73907876d509703a
nerdogan <nerdogan@gcmlp.com>
2023-07-06 15:28:51 -0500

    add bmrk definition, clean df, xs EMM rank %iles

--- AzureFunctions/_legacy/Reports/reports/performance_quality/xpfund_high_pq_screen.py
+++ AzureFunctions/_legacy/Reports/reports/performance_quality/xpfund_high_pq_screen.py
@@ -1,13 +1,15 @@
 import datetime as dt
 import json
 import pandas as pd
+import numpy as np
 from gcm.Dao.DaoRunner import DaoRunnerConfigArgs, DaoRunner
 from gcm.Dao.DaoSources import DaoSource
 from gcm.Dao.daos.azure_datalake.azure_datalake_dao import AzureDataLakeDao
 from gcm.inv.scenario import Scenario
 from gcm.inv.dataprovider.investment_group import InvestmentGroup
 #from gcm.inv.models.highlow_pq_screen.highlow_xpfund_firmwide import _xpfund_data_to_highlow_df
-from _legacy.Reports.reports.performance_quality.tmp import _xpfund_data_to_highlow_df,_5y_arb_xs_emm_percentiles
+from _legacy.Reports.reports.performance_quality.tmp import _3y_arb_xs_analysis,_3y_arb_xs_emm_percentiles
+from _legacy.Reports.reports.performance_quality.tmp import _xpfund_data_to_highlow_df,_5y_arb_xs_emm_percentiles, _clean_firmwide_xpfund_data
 from functools import partial, cached_property
 from _legacy.Reports.reports.performance_quality.helper import PerformanceQualityHelper
 from _legacy.core.reporting_runner_base import (
@@ -38,9 +40,12 @@ class XPfundHighPQScreen(ReportingRunnerBase):
     
     @cached_property
     def _xpfund_pq_report(self):       
-        #as_of_date = self._as_of_date.strftime("%Y-%m-%d")
+        as_of_date = self._as_of_date.strftime("%Y-%m-%d")
         #file = self._fund_name.replace("/", "") + "_fund_inputs_" + as_of_date + ".json"
-        inputs = PerformanceQualityHelper().download_inputs(location="raw/investmentsreporting/summarydata/xpfund_performance_quality", file_path="_firm_x_portfolio_fund2023-03-31.json")
+        inputs = PerformanceQualityHelper().download_inputs(
+            location="raw/investmentsreporting/summarydata/xpfund_performance_quality", 
+            file_path="_firm_x_portfolio_fund"+as_of_date+".json"
+            )
         return inputs
         
     
@@ -66,19 +71,25 @@ class XPfundHighPQScreen(ReportingRunnerBase):
     def generate_xpfund_high_pq_screen_data(self):
         with Scenario(dao=self._runner, as_of_date=self._as_of_date).context():
             date_q_minus_1 = pd.to_datetime(self._as_of_date - pd.tseries.offsets.QuarterEnd(1)).date()
-            y = self._get_xpfund_pq_report(runner=self._runner, as_of_date=date_q_minus_1)
-            m=y["('AbsoluteReturnBenchmarkExcess', '3Y')"]
-            #y['x']=y["('AbsoluteReturnBenchmarkExcess', '5Y')"]
-            #y['5y_ptiles'] = y.x.rank(numeric_only=True, pct = True)
+            firm_xpfund_report_data = self._get_xpfund_pq_report(runner=self._runner, as_of_date=date_q_minus_1)
+            m=firm_xpfund_report_data["('AbsoluteReturnBenchmarkExcess', '3Y')"]
+            firm_xpfund_highlow_df=firm_xpfund_report_data.copy()
+            firm_xpfund_highlow_df=_xpfund_data_to_highlow_df(firm_xpfund_highlow_df)
+            
+            # firm_xpfund_report_data=firm_xpfund_report_data.copy()
+            # firm_xpfund_report_data=_clean_firmwide_xpfund_data(firm_xpfund_report_data)
+            # firm_xpfund_report_data=_3y_arb_xs_analysis(firm_xpfund_report_data)
+            # firm_xpfund_report_data=_3y_arb_xs_emm_percentiles(firm_xpfund_report_data)
+            # firm_xpfund_report_data=_5y_arb_xs_emm_percentiles(firm_xpfund_report_data)
+
             #n=_5y_arb_xs_emm_percentiles(y)
             #g=y['5y_ptiles']
-            z=_xpfund_data_to_highlow_df(y)
             #call fxn that sends y to inv models to manipulate it -> z
             #call fxn that sends z to inv models to break into high and low
             #input data, and input data json
             #write to datalake
 
-        return 
+        return firm_xpfund_highlow_df
     
     def run(self, **kwargs):
         try:
