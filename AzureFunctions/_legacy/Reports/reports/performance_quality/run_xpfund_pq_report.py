4ad412f7f44c861b5f6bd068b86a89a61ef5f904
nerdogan <nerdogan@gcmlp.com>
2023-07-17 11:49:13 -0500

    run highlow w/ firmwide xpfund, format

--- AzureFunctions/_legacy/Reports/reports/performance_quality/run_xpfund_pq_report.py
+++ AzureFunctions/_legacy/Reports/reports/performance_quality/run_xpfund_pq_report.py
@@ -4,6 +4,7 @@ import pandas as pd
 import datetime as dt
 
 from _legacy.Reports.reports.performance_quality.xpfund_pq_report import generate_xpfund_pq_report_data
+from _legacy.Reports.reports.performance_quality.xpfund_highlow_pq_screen import XPfundHighLowPQScreen
 from _legacy.core.ReportStructure.report_structure import (
     ReportingEntityTypes,
     ReportType,
@@ -19,19 +20,16 @@ from _legacy.core.reporting_runner_base import (
 from gcm.Dao.daos.azure_datalake.azure_datalake_dao import AzureDataLakeDao
 from gcm.Dao.DaoRunner import DaoRunner, DaoRunnerConfigArgs, DaoSource
 from gcm.inv.scenario import Scenario
-from _legacy.Reports.reports.performance_quality.helper import PerformanceQualityHelper
-from functools import partial, cached_property
 
 
 class RunXPFundPqReport(ReportingRunnerBase):
     def __init__(self, runner, as_of_date):
-        super().__init__(runner=runner)
+        super().__init__(runner=dao_runner)
         self._as_of_date = as_of_date
         self._underlying_data_location = "raw/investmentsreporting/summarydata/xpfund_performance_quality"
         #self._helper = PerformanceQualityHelper()
         #self._dao = Scenario.get_attribute("dao")
-    
-    
+
     def generate_report(self, inv_group_ids=None, additional_ids=None,
                         custom_report_name=None, write_to_reporting_hub=False):
         #x=self._fund_inputs
@@ -44,14 +42,14 @@ class RunXPFundPqReport(ReportingRunnerBase):
         report_data.loc[report_data['InvestmentGroupName'] == 'D1 Capital', 'InvestmentGroupName'] = 'D1 - GIP'
         report_data.loc[report_data['InvestmentGroupName'] == 'D1 Liquid Class', 'InvestmentGroupName'] \
             = 'D1 - Publics Only'
-        report_data_orig=report_data.drop('absolute_return_benchmark', axis=1)
-        report_data_orig=report_data_orig.drop(('AbsoluteReturnBenchmarkExcessLag', '3Y'), axis=1)
-        report_data_orig=report_data_orig.drop(('AbsoluteReturnBenchmarkExcessLag', 'ITD'), axis=1)
+        report_data_orig = report_data.drop('absolute_return_benchmark', axis=1)
+        report_data_orig = report_data_orig.drop(('AbsoluteReturnBenchmarkExcessLag', '3Y'), axis=1)
+        report_data_orig = report_data_orig.drop(('AbsoluteReturnBenchmarkExcessLag', 'ITD'), axis=1)
         input_data = {
             'as_of_date': pd.DataFrame({'date': [self._as_of_date]}),
             'report_data': report_data_orig
         }
-        
+
         input_data_json = {
             'as_of_date': pd.DataFrame({'date': [self._as_of_date]}).to_json(orient="index"),
             'report_data': report_data.to_json(orient="index"),
@@ -66,18 +64,6 @@ class RunXPFundPqReport(ReportingRunnerBase):
         else:
             report_name = custom_report_name
 
-        #write to json
-        # firm_input = json.dumps(report_data)
-        # write_params = AzureDataLakeDao.create_get_data_params(
-        #     self._underlying_data_location,
-        #     "_firm_x_portfolio_fund" + self._as_of_date + ".json",
-        #     retry=False,
-        # )
-        # self._runner.execute(
-        #     params=write_params,
-        #     source=DaoSource.DataLake,
-        #     operation=lambda dao, params: dao.post_data(params, firm_input),
-        # )
         data_to_write = json.dumps(input_data_json)
         as_of_date = self._as_of_date.strftime("%Y-%m-%d")
         write_params = AzureDataLakeDao.create_get_data_params(
@@ -90,10 +76,9 @@ class RunXPFundPqReport(ReportingRunnerBase):
             source=DaoSource.DataLake,
             operation=lambda dao, params: dao.post_data(params, data_to_write),
         )
-        
+
         logging.info("JSON stored to DataLake")
-        
-        
+
         if write_to_reporting_hub:
             with Scenario(as_of_date=date).context():
                 InvestmentsReportRunner().execute(
@@ -130,12 +115,20 @@ class RunXPFundPqReport(ReportingRunnerBase):
                     # output_dir="cleansed/investmentsreporting/printedexcels/",
                     report_output_source=DaoSource.DataLake
                 )
+        #self.generate_highlow_report
+
+    def generate_highlow_report(self):
+        high_low_report = XPfundHighLowPQScreen(
+            as_of_date=self._as_of_date
+        )
+        return high_low_report.execute()
 
     def run(self, inv_group_ids=None, **kwargs):
         self.generate_report(inv_group_ids=inv_group_ids,
                              additional_ids=kwargs.get('additional_ids'),
                              custom_report_name=kwargs.get('custom_report_name'),
                              write_to_reporting_hub=kwargs.get('write_to_reporting_hub', True))
+        self.generate_highlow_report()
         return "Complete"
 
 
@@ -157,8 +150,8 @@ if __name__ == "__main__":
                         "Subscription": "prd",
                     },
                     DaoSource.DataLake_Blob.name: {
-                        "Environment": "dev",
-                        "Subscription": "nonprd",
+                        "Environment": "prd",
+                        "Subscription": "prd",
                     },
                     DaoSource.ReportingStorage.name: {
                         "Environment": "dev",
@@ -168,11 +161,14 @@ if __name__ == "__main__":
             })
 
     date = dt.date(2023, 4, 30)
-    with Scenario(as_of_date=date).context():
-        report = RunXPFundPqReport(runner=dao_runner, as_of_date=date)
+    with Scenario(runner=dao_runner, as_of_date=date).context():
+
+        report_runner = RunXPFundPqReport(runner=dao_runner, as_of_date=date)
 
         # firmwide report. # add in D1 Liquid Class which isn't an EMM
-        report.execute(additional_ids=[23447])
+        report_runner.execute(additional_ids=[23447])
+
+        #report_runner.generate_highlow_report()
 
         # esg report
         # custom_report_name = "ARS Performance Quality - ESG x Portfolio Fund"
